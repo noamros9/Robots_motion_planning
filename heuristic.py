c4b1ad151d2684302370e31512c2a9975d98044e
Dav-Dude <63969858+Dav-Dude@users.noreply.github.com>
2020-09-08 11:21:04 +0300

    Update heuristic.py
    creating subgraph nodes for johnson

--- heuristic.py
+++ heuristic.py
@@ -2,6 +2,7 @@ from arr2_epec_cs_ex import *
 import networkx as nx
 import Collision_detection
 import conversions
+import random
 
 class Heuristic():
     def __init__(self, graphs_single_robot, all_pairs_shortest_paths_per_robot):
@@ -13,12 +14,7 @@ def calc_heur(heuristic_obj, i, v_near, neighbor, objective,coupons,r):
     G = heuristic_obj.graphs_single_robot[i]
     v_near_to_neighbor = 0
     if v_near != neighbor and v_near != None:
-        coupcord = [coupon[0] for coupon in coupons]
-        tup = Collision_detection.is_collision_robot_coupons(coupcord, conversions.point_d_to_point_2(v_near), r)
-        if tup[0] == True:
-            v_near_to_neighbor = G[v_near][neighbor]['weight'] #- tup[1][1]*1000
-        else:
-            v_near_to_neighbor = G[v_near][neighbor]['weight']
+        v_near_to_neighbor = G[v_near][neighbor]['weight']
 
     neighbor_to_objective_path_len = 0
     path = heuristic_obj.all_pairs_shortest_paths_per_robot[i][neighbor][objective]
@@ -30,6 +26,7 @@ def calc_heur(heuristic_obj, i, v_near, neighbor, objective,coupons,r):
 
 def compute_all_pair_shortest_path(graphs_single_robot):
     all_pairs_shortest_paths_per_robot = []
+    subgraph_nodes = random.sample(graphs_single_robot.nodes,len(graphs_single_robot.nodes)/10)
     for i in range(len(graphs_single_robot)):
         all_pairs_shortest_paths_per_robot.append(\
             nx.johnson(graphs_single_robot[i], weight='weight'))
